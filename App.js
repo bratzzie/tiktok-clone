6b4b8b78f4e9f648b433d47819f701b886c1da28
Bichurina Anastasiia <69593629+bratzzie@users.noreply.github.com>
2021-03-29 22:49:53 +0300

    :construction_worker: auth

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import "react-native-gesture-handler";
 import RootNavigation from "./src/navigation";
+import { withAuthenticator } from "aws-amplify-react-native";
 import Amplify from "aws-amplify";
 import config from "./src/aws-exports";
+import { Auth, API, graphqlOperation } from "aws-amplify";
+import { createUser } from "./src/graphql/mutations";
+import { getUser } from "./src/graphql/queries";
 Amplify.configure(config);
-export default function App() {
+function App() {
+  useEffect(() => {
+    const fetchUser = async () => {
+      //get current user
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+
+      //check the existance
+
+      if (!userInfo) {
+        return;
+      }
+
+      const getUserRes = await API.graphql(
+        graphqlOperation(getUser, { id: userInfo.attributes.sub })
+      );
+
+      if (getUserRes.data.getUser) {
+        console.log("User already exists!");
+        return;
+      }
+      // then -> add new user
+      let URL = `https://avatars.dicebear.com/api/bottts/${userInfo.username}.svg`;
+      const newUser = {
+        id: userInfo.attributes.sub,
+        username: userInfo.username,
+        email: userInfo.attributes.email,
+        imageURL: URL,
+      };
+
+      await API.graphql(graphqlOperation(createUser, { input: newUser }));
+    };
+
+    fetchUser();
+  }, []);
   return (
     <>
       <StatusBar style="auto" />
@@ -25,3 +64,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+export default withAuthenticator(App);
